8047e3864a2749c492e805e45c4a7334df7b0fac
Chi Ho <>
2020-07-15 21:58:28 +0200

    first commit

---
+++ client/src/components/Posts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="posts">
+    ABN AMRO assignment
+  </div>
+</template>
+
+<script>
+import PostsService from '@/services/PostsService'
+export default {
+  name: 'posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data.posts
+    }
+  }
+}
+</script>
+<style>
+.posts {
+  color: black;
+}
+</style>
