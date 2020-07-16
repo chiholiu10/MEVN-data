49c646e35871b5ce7e68cf4f84e055cb0f0680b9
Chi Ho <>
2020-07-16 21:53:20 +0200

    axios added

--- server/src/app.js
+++ server/src/app.js
@@ -2,83 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
 const app = express()
+const axios = require('axios')
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 const mongodb_conn_module = require('./mongodbConnModule');
-
 var db = mongodb_conn_module.connect();
 
 var Post = require("../models/post");
 
-app.get('/posts', (req, res) => {
-  Post.find({}, 'title description', function (error, posts) {
-	  if (error) { console.error(error); }
-	  res.send({
-			posts: posts
+app.get('/', (req, res) => {
+	let data;
+	axios.get('https://gist.githubusercontent.com/hurkanakbiyik/5d54addf62f2c4a59c1e9222e3dc2d08/raw/f85c3ddb9b218515b88eb8f723fc05f257bed468/nerf-herders-test-data')
+		.then(response => {
+			res.json(response.data)
 		})
-	}).sort({_id:-1})
-})
-
-app.post('/add_post', (req, res) => {
-	var db = req.db;
-	var title = req.body.title;
-	var description = req.body.description;
-	var new_post = new Post({
-		title: title,
-		description: description
-	})
-
-	new_post.save(function (error) {
-		if (error) {
-			console.log(error)
-		}
-		res.send({
-			success: true
+		.catch(error => {
+			console.log(error);
 		})
-	})
-})
-
-app.put('/posts/:id', (req, res) => {
-	var db = req.db;
-	Post.findById(req.params.id, 'title description', function (error, post) {
-	  if (error) { console.error(error); }
-
-	  post.title = req.body.title
-	  post.description = req.body.description
-	  post.save(function (error) {
-			if (error) {
-				console.log(error)
-			}
-			res.send({
-				success: true
-			})
-		})
-	})
-})
-
-app.delete('/posts/:id', (req, res) => {
-	var db = req.db;
-	Post.remove({
-		_id: req.params.id
-	}, function(err, post){
-		if (err)
-			res.send(err)
-		res.send({
-			success: true
-		})
-	})
-})
-
-app.get('/post/:id', (req, res) => {
-	var db = req.db;
-	Post.findById(req.params.id, 'title description', function (error, post) {
-	  if (error) { console.error(error); }
-	  res.send(post)
-	})
 })
 
 app.listen(process.env.PORT || 8082)
