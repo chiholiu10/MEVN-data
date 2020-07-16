90ab710ab1bcefd1244ef85119943210583e26a8
Chi Ho <>
2020-07-16 21:09:12 +0200

    hierarchy repository

---
+++ server/repositories/hierarchyRepository.js
@@ -0,0 +1,19 @@
+const neo4j = require('neo4j-driver');
+const config = require('../config.json');
+console.log('config' + config)
+module.exports.getAll = async () => {
+    const driver = neo4j.driver(config.url, neo4j.auth.basic(config.user, config.pass));
+    const session = driver.session();
+    const results = await session.run(`
+        MATCH p=(n:Node)-[:PARENT_TO*]->(m)
+        WHERE NOT (:Node)-[:PARENT_TO]->(n)
+        WITH COLLECT(p) AS ps
+        CALL apoc.convert.toTree(ps) yield value
+        RETURN value
+    `);
+
+    await driver.close();
+
+    // eslint-disable-next-line no-underscore-dangle
+    return results.records.map((x) => x._fields);
+};
