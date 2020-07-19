738f822e88037aca8350998ccc73eed2f6f55870
Chi Ho <>
2020-07-20 00:57:56 +0200

    imported babel/polyfill and styles.css

--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueSweetalert2 from 'vue-sweetalert2'
+import App from './App.vue'
+import '@babel/polyfill'
+import './styles.scss'
 
-Vue.use(VueSweetalert2)
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+// eslint-disable-next-line no-new
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
