3f9d20e662e26a05b124b9bd44746e8b42849796
Chi Ho <>
2020-07-20 23:54:45 +0200

    mock axios call and removed classname

--- client/src/components/DataField.spec.js
+++ client/src/components/DataField.spec.js
@@ -1,9 +1,60 @@
 import {
-    mount
+    mount,
+    shallowMount
 } from '@vue/test-utils'
 import DataField from './DataField.vue'
 
+jest.mock("axios");
+
+jest.mock("axios", () => ({
+    get: () => Promise.resolve({
+        data: [{
+                "name": "A",
+                "description": "This is a description of A",
+                "parentId": ""
+            },
+            {
+                "name": "B",
+                "description": "This is a description of B",
+                "parent": "A"
+            },
+            {
+                "name": "C",
+                "description": "This is a description of C",
+                "parent": "A"
+            },
+            {
+                "name": "D",
+                "description": "This is a description of D",
+                "parent": "A"
+            },
+            {
+                "name": "B-1",
+                "description": "This is a description of B-1",
+                "parent": "B"
+            },
+            {
+                "name": "B-2",
+                "description": "This is a description of B-2",
+                "parent": "B"
+            },
+            {
+                "name": "B-3",
+                "description": "This is a description of B-3",
+                "parent": "B"
+            }
+        ]
+    })
+}));
+
 describe('DataField', () => {
+    test('check length of list data', () => {
+        const wrapper = shallowMount(DataField)
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.listData.length).toBe(7);
+        });
+    });
+
     test('should mount', () => {
         const wrapper = mount(DataField)
         expect(wrapper.isVueInstance()).toBeTruthy()
